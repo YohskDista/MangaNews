b60046df5a09d834e0d4d195f88739064fd63dbd
YohskDiSta <yohsk10@gmail.com>
2024-07-14 18:18:59 +0200

    Split in multiple files

---
+++ src/MangaNews.Core/Chapter.cs
@@ -0,0 +1,21 @@
+﻿namespace MangaNews.Core;
+
+public sealed record Chapter
+{
+    public Chapter(string title, string releaseTime, string link)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
+        ArgumentException.ThrowIfNullOrWhiteSpace(releaseTime);
+        ArgumentException.ThrowIfNullOrWhiteSpace(link);
+
+        Title = title;
+        ReleaseTime = releaseTime;
+        Link = link;
+    }
+
+    public string Title { get; set; }
+
+    public string ReleaseTime { get; set; }
+
+    public string Link { get; set; }
+}
